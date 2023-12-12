8d6a7b0bf55a988b5b9665db15fc8f85eacf2858
wyshim319@gmail.com <wyshim319@gmail.com>
2023-12-12 16:00:12 +0900

    231212 : git commit and push test

---
+++ C/Test/23_12/231212_Test2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "git에 올리는 방법 : " << std::endl;
+	std::cout << "1. Do Something..." << std::endl;
+	std::cout << "2. <Git 변경 내용> 창 켜기" << std::endl;
+	std::cout << "3. 커밋 메시지 작성" << std::endl;
+
+	return 0;
+}
