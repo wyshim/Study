82c5290536bc8f7b4961887b3f0eb3dd26ae5587
wyshim319@gmail.com <wyshim319@gmail.com>
2023-12-12 16:03:27 +0900

    231212 : 깃 사용법 추가

--- C/Test/23_12/231212_Test2.cpp
+++ C/Test/23_12/231212_Test2.cpp
@@ -6,6 +6,8 @@ int main()
 	std::cout << "1. Do Something..." << std::endl;
 	std::cout << "2. <Git 변경 내용> 창 켜기" << std::endl;
 	std::cout << "3. 커밋 메시지 작성" << std::endl;
+	std::cout << "4. <모두 커밋> 버튼 누르기 - 커밋 성공인지 확인" << std::endl;
+	std::cout << "5. <푸쉬>까지 해야 실제 원격 git에 업로드 된 것" << std::endl;
 
 	return 0;
 }
