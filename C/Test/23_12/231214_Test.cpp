486849879ef8a28632a4d029208cf75604c2d9a4
wyshim319@gmail.com <wyshim319@gmail.com>
2023-12-14 19:26:09 +0900

    소수점 이하 두자리수만 출력

--- C/Test/23_12/231214_Test.cpp
+++ C/Test/23_12/231214_Test.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <iomanip>
+int main()
+{
+	double f;
+	std::cin >> f;
+	int i = 100*(f - (int)f);
+	std::cout << i;
+	return 0;
+}
 
+
+
+
+/*
 bool is_prime_number(int x)
 {
 	for (int i = 2; i<int(sqrt(x)); i++)
@@ -27,3 +40,4 @@ int main()
 	std::cout << num << " = " + result.substr(0, result.length()-2);
 	
 }
+*/
