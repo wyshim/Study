4b9544692b8b7ba4bd4a8b381d379cdc96c8a4df
wyshim319@gmail.com <wyshim319@gmail.com>
2023-12-19 21:59:53 +0900

    2차원 배열, 포인터

---
+++ C/Test/23_12/19_Test.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+void pswap(int** pa, int** pb)
+{
+	int* temp = *pb;
+	*pb = *pa;
+	*pa = temp;
+}
+
+int max(int, int);
+int main()
+{
+	/*
+	int a=2, b;
+	int *pa, *pb;
+	int** ppa, ** ppb;
+	pa = &a;
+	pb = &b;
+	ppa = &pa;
+	ppb = &pb;
+	//std::cout << pa << " " << pb << std::endl;
+	//pswap(&pa, &pb);
+	//std::cout << pa << " " << pb << std::endl;
+	std::cout << ppa << " " << &pa << std::endl;
+	std::cout << *ppa << " " << pa << std::endl;
+	std::cout << **ppa << " " << *pa << std::endl;
+	*/
+
+	/*
+	int a=10, b=20;
+	int (*pmax)(int, int); // 함수 포인터
+	pmax = max; 
+	
+	std::cout << max(a, b) << " " << pmax(a, b);
+	return 0;
+	*/
+
+	char a, b, c;
+	a = 100;
+	b = 300;
+	c = a + b;
+	std::cout << (int)a << " + " << (int)b << " = " << (int)c;
+}
+
+int max(int a, int b)
+{
+	if (a > b) return a;
+	else return b;
+}
